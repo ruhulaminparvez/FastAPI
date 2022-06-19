29dd51316df657d946c153e22e4dc80c1d519584
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-19 23:18:15 +0600

    Basic SetUp

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index():
+    return {'fakir gusti': {'name': 'fakir mahamud', 'age': '24'}}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
