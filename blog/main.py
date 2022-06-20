2f90dbd931ced95e3f790cd77702c9a427c50f73
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-20 17:43:35 +0600

    Get,Post request applied and store info in Database

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI
-from . import schemas
+from fastapi import FastAPI, Depends
+from blog.database import SessionLocal
+from . import schemas, models
+from .database import engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def index(request: schemas.Blog):
-    return request
+def index(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog') 
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
